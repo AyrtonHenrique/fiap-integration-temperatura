d6713acd521e9d3ae22b739d391a282c1a7e6651
ayrton <ayrtonhe@live.com>
2020-10-18 15:11:15 -0300

    initial commit

---
+++ fiap-integration-microservice/src/main/java/com/fiap/integration/fiapintegrationmicroservice/FiapIntegrationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.integration.fiapintegrationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapIntegrationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapIntegrationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ fiap-integration-microservice/src/test/java/com/fiap/integration/fiapintegrationmicroservice/FiapIntegrationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.integration.fiapintegrationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapIntegrationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
