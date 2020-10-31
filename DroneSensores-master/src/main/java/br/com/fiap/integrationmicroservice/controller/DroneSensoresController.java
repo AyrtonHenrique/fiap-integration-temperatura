deae94a5f852e84d66ff19998710b0f1239ad9a5
Carlos Eduardo Roque da Silva <ceduardo.roque@gmail.com>
2020-10-31 08:31:06 -0300

    Correções diversas - Correções nas validações de entrada dos JSON - Melhoria das mensagens de log

--- DroneSensores-master/src/main/java/br/com/fiap/integrationmicroservice/controller/DroneSensoresController.java
+++ DroneSensores-master/src/main/java/br/com/fiap/integrationmicroservice/controller/DroneSensoresController.java
@@ -26,6 +26,7 @@ public class DroneSensoresController {
 
 	private final Logger logger = LoggerFactory.getLogger(DroneSensoresController.class);
     private final DroneService droneService;
+    private long mensagensRecebidas = 0; 
     
     public DroneSensoresController(DroneService droneService) {
         this.droneService = droneService;
@@ -33,51 +34,65 @@ public class DroneSensoresController {
 
     @PostMapping
     public ResponseEntity<Void> create(@RequestBody DroneCreateDTO droneCreateDTO) {
+    	String tokenNumeroMensagemRecebida = getProximaMensagemRecebida();
         try {
-        	        
+        	logger.info(tokenNumeroMensagemRecebida + "Nova medição recebida. Iniciando processamento.");
         	Class<DroneCreateDTO> droneCreate  = DroneCreateDTO.class;
         	Field[] campos = droneCreate.getDeclaredFields();
-        	
         	for (Field campo : campos) {
         		campo.setAccessible(true);
         		Object objeto = campo.get(droneCreateDTO);
-        		if (objeto==null || objeto.equals("") ) {
-        			logger.error("Erro Post com o envio dados do drone - Favor preencher os atributos");
+        		if (objeto == null || objeto.equals("") ) {
+        			logger.error(tokenNumeroMensagemRecebida + "Dados enviados inconsistentes. Cancelando processamento.");
         			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-        			
         		} 
-        		
         	}
+        	logger.info(tokenNumeroMensagemRecebida + "Validação dos dados enviados concluída. Enviando dados para o RabbitMQ para processamento.");
 			droneService.send(droneCreateDTO);
-			logger.info("Post com o envio dados do drone - Dados Prechidos");
+			logger.info(tokenNumeroMensagemRecebida + "Dados enviados com sucesso.");
 			return new ResponseEntity<Void>(HttpStatus.OK);
-        	
-		} catch (Exception e) {
+		
+        } catch (Exception e) {
+        	logger.error(tokenNumeroMensagemRecebida + "Ocorreu um problema no processamento dos dados enviados", e);
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 		}
-   
+ 
     }
     
     
     
-    @PostMapping("{id}/medicoes")
+    private String getProximaMensagemRecebida() {
+		this.mensagensRecebidas++;
+		return "[" + this.mensagensRecebidas + "] ";
+	}
+
+	@PostMapping("{id}/medicoes")
     public ResponseEntity<Void> createMedicoes(@PathVariable String id,  @RequestBody DroneMedicoesCreateDTO droneMedicoesCreateDTO){
-        try {
+		String tokenNumeroMensagemRecebida = getProximaMensagemRecebida();
+		try {
+			logger.info(tokenNumeroMensagemRecebida + "Nova medição recebida. Iniciando processamento.");
         	Class<DroneMedicoesCreateDTO> droneCreate  = DroneMedicoesCreateDTO.class;
         	Field[] campos = droneCreate.getDeclaredFields();
         	for (Field campo : campos) {
         		campo.setAccessible(true);
         		Object objeto = campo.get(droneMedicoesCreateDTO);
-        		if (objeto == null || (objeto.equals("") && (id.equals("0") || id.equals(null) ||id.equals("") )) ) {
-        			logger.error("Problema nos campos de para Post com o envio dados das medicoes do drone - Favor preencher os dados");
+        		if (objeto == null || (objeto.equals("") && id.equals("0")) ) {
+        			logger.error(tokenNumeroMensagemRecebida + "Dados enviados inconsistentes. Cancelando processamento.");
         			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-        			} 
-        	}
-        	 droneMedicoesCreateDTO.setIdDrone(id);
-			 droneService.sendMedicoes(droneMedicoesCreateDTO);
-			 logger.info("Dados de medicoes do drone enviado.");
-			 return new ResponseEntity<Void>(HttpStatus.OK);
-		} catch (Exception e) {
+        		} 
+        	}		
+        	if (id == null || id.equals("") ) {
+				droneMedicoesCreateDTO.setIdDrone("");
+			} else { 
+				droneMedicoesCreateDTO.setIdDrone(id);
+			}
+        	logger.info(tokenNumeroMensagemRecebida + "Validação dos dados enviados concluída. Enviando dados de medição para o RabbitMQ para processamento.");
+			droneService.sendMedicoes(droneMedicoesCreateDTO);
+			logger.info(tokenNumeroMensagemRecebida + "Dados de medição enviados com sucesso.");
+			return new ResponseEntity<Void>(HttpStatus.OK);
+	
+        } catch (Exception e) {
+        	logger.error(tokenNumeroMensagemRecebida + "Ocorreu um problema no processamento dos dados enviados", e);
 			return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 		}
        
