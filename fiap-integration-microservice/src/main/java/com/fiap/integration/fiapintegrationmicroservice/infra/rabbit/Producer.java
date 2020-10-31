c5e04747e977e1128e3297b2b01a96ee0e110f02
Carlos Eduardo Roque da Silva <ceduardo.roque@gmail.com>
2020-10-31 09:51:10 -0300

    Ajustes para envio para o exchange
    - Ajustes para envio para o exchange passando a routing key, e não para
    uma queue diretamente

--- fiap-integration-microservice/src/main/java/com/fiap/integration/fiapintegrationmicroservice/infra/rabbit/Producer.java
+++ fiap-integration-microservice/src/main/java/com/fiap/integration/fiapintegrationmicroservice/infra/rabbit/Producer.java
@@ -1,6 +1,5 @@
 package com.fiap.integration.fiapintegrationmicroservice.infra.rabbit;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
@@ -14,13 +13,10 @@ public class Producer {
     private String exchangeName;
     private String routingKey;
     
-    private Queue queue;
-    
     public Producer(Environment environment) {
 		this.environment = environment;
-		this.exchangeName = environment.getProperty("queue.email.nome");
-		this.routingKey= environment.getProperty("queue.email.routing-key");
-		this.queue = new Queue(exchangeName, true); 
+		this.exchangeName = environment.getProperty("spring.rabbitmq.template.exchange");
+		this.routingKey= environment.getProperty("spring.rabbitmq.template.routing-key");
     }
     
     public void send(String email) {
