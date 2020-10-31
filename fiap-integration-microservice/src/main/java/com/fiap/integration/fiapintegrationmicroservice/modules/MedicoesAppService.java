fe5d472cbf613d546fbb47df221e7e9e33df11e8
Carlos Eduardo Roque da Silva <ceduardo.roque@gmail.com>
2020-10-31 09:50:11 -0300

    Inclusão de logs
    - Inclusão de logs de acompanhamento para evidenciar as regras e testar
    envio do email

--- fiap-integration-microservice/src/main/java/com/fiap/integration/fiapintegrationmicroservice/modules/MedicoesAppService.java
+++ fiap-integration-microservice/src/main/java/com/fiap/integration/fiapintegrationmicroservice/modules/MedicoesAppService.java
@@ -1,11 +1,13 @@
 package com.fiap.integration.fiapintegrationmicroservice.modules;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,13 +19,9 @@ import com.fiap.integration.fiapintegrationmicroservice.repositories.MedicoesRep
 import com.fiap.integration.fiapintegrationmicroservice.viewModels.EmailProducerRequest;
 import com.fiap.integration.fiapintegrationmicroservice.viewModels.MedicaoConsumerResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 @Service
 public class MedicoesAppService implements IMedicoesAppService {
 
-    // private static LocalDateTime dataExp = LocalDateTime.now().plusMinutes(1);
     @Autowired
     private Producer producer;
 
@@ -32,18 +30,19 @@ public class MedicoesAppService implements IMedicoesAppService {
 
     @Autowired
     private DroneRepository _droneRepository;
-
+    private final Logger logger = LoggerFactory.getLogger(MedicoesAppService.class);
     private static List<Evento> _eventoAlerta = new ArrayList<Evento>();
 
     @Override
     public void Analisar(MedicaoConsumerResponse medicaoResponse) throws JsonProcessingException {
 
+    	logger.info("Atualizando Drone no banco de dados");
         Drone drone = _droneRepository.save(new Drone(medicaoResponse.idDrone, ""));
 
         Medicao medicao = new Medicao(drone, medicaoResponse.latitude, medicaoResponse.longitude,
                 medicaoResponse.temperatura, medicaoResponse.umidade, medicaoResponse.dataAtualizacao,
                 medicaoResponse.rastreamento);
-
+    	logger.info("Salvando nova Medição no banco de dados");
         _medicoesRepository.save(medicao);
 
         Optional<Drone> _drone = _droneRepository.findById(medicao.getDrone().getId());
@@ -52,11 +51,13 @@ public class MedicoesAppService implements IMedicoesAppService {
                 medicao = _drone.get().getMedicoes().stream().findFirst().get();
             }
         }
+
+    	logger.info("Iniciando validação de alertas de temperatura e umidade...");
         Long iddrone = medicao.getDrone().getId();
         Optional<Evento> evento = _eventoAlerta.stream().filter(e -> e.getIdDrone() == iddrone).findFirst();
 
         if ((medicao.getTemperatura() >= 35 || medicao.getTemperatura() <= 0) || (medicao.getUmidade() <= 0.15)) {
-
+        	logger.info("Alerta de Temperatura e Umidade ATIVADO. Validando regras...");
             if (_eventoAlerta.isEmpty() || !evento.isPresent()) {
                 iniciarAvalicaoUmMinuto(medicao);
                 return;
@@ -64,7 +65,10 @@ public class MedicoesAppService implements IMedicoesAppService {
 
             if (evento.get().getHoraFimEnvioAlerta().isBefore(medicao.getDataAtualizacao())
                     && evento.get().getPersiteTemperaturaUmidadeNivelAlerta()) {
+            	logger.info("Alerta de Temperatura e Pressão - NECESSIDADE DE ENVIO DE E-MAIL PARA GESTORES.");
+            	logger.info("Alerta de Temperatura e Pressão - Temperatura ou Umidade fora dos limites aceitos no período de 1min.");
                 notificar(medicao);
+                logger.info("Alerta de Temperatura e Pressão - Solicitação de envio de EMAIL encaminhada.");
                 _eventoAlerta.remove(evento.get());
                 return;
             }
