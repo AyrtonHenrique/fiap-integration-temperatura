137f2fc91f41f9a6371fdcfae06463aa087b003b
Carlos Eduardo Roque da Silva <ceduardo.roque@gmail.com>
2020-10-31 09:45:40 -0300

    Ajuste no Produtor
    - Ajustes no produtor de mensagens para de mensagens para enviar
    mensagens para o exchange e nao para a queue
    - Ajuste para enviar passando a routing key
    - Injeção das variáveis de ambiente do properties

--- fiap-integration-microservice/src/main/java/com/fiap/integration/fiapintegrationmicroservice/infra/rabbit/Producer.java
+++ fiap-integration-microservice/src/main/java/com/fiap/integration/fiapintegrationmicroservice/infra/rabbit/Producer.java
@@ -3,18 +3,27 @@ package com.fiap.integration.fiapintegrationmicroservice.infra.rabbit;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Producer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
- 
-    static final String queueName = "${queue.email.nome}";
-
-    private Queue queue = new Queue(queueName, true);
- 
+	private Environment environment;
+    private String exchangeName;
+    private String routingKey;
+    
+    private Queue queue;
+    
+    public Producer(Environment environment) {
+		this.environment = environment;
+		this.exchangeName = environment.getProperty("queue.email.nome");
+		this.routingKey= environment.getProperty("queue.email.routing-key");
+		this.queue = new Queue(exchangeName, true); 
+    }
+    
     public void send(String email) {
-        rabbitTemplate.convertAndSend(this.queue.getName(), email);
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, email);
     }
 }
